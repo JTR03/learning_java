df1e036b089e7a5a77069b9ec84c32af77f47f47
Jonathan Tshikala <jonathanrose606@gmail.com>
2022-04-10 08:51:08 +0200

    declaring variables

---
+++ variable.java
@@ -0,0 +1,15 @@
+public class variable {
+   public static void main(String[] args) {
+       int age = 15;
+       double GPA = 3.15;
+       char firstInitial = 'J';
+       char lastInitial = 'T';
+       boolean perfectAttendance = true;
+       
+       System.out.println(age);
+       System.out.println(GPA);
+       System.out.println(firstInitial);
+       System.out.println(lastInitial);
+       System.out.println(perfectAttendance);
+   } 
+}
